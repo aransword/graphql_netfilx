ffafd1339f983e2f9d0d32b134311a601403a239
aransword <lupin521@gmail.com>
2026-03-18 10:35:59 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# GraphQL
+**Netflix DGS**를 이용해서 GraphQL을 지원하는 Supergraph Architecture를 구현한 프로젝트입니다.
---
+++ review_service/src/test/java/com/netflix/review_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.review_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
